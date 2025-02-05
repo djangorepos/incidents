d27d8f8e1dec9dee50d3b3d173e1029566a9e572
Tim <ex.timwidow@gmail.com>
2025-02-05 11:35:09 +0200

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
+# Create database migrations
+echo "Create database migrations"
+python manage.py makemigrations
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
